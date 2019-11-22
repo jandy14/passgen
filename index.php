f4009fcfb89f4ea538cc4cf278f0fbd7239f67d1
jandy14 <seonguk14@changwon.ac.kr>
2019-11-22 12:46:08 +0900

    title change

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Password Generator</title>
   </head>
   <body>
     <h2>컴퓨터께서 행운의 숫자를 주실지니!!</h2>
